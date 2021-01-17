93074ea8fe161d4d0187f58a3bbabd3072aeae60
sjs <seo777111@gmail.com>
2021-01-17 14:58:53 +0900

    another resolve printer

--- 프린터.js
+++ 프린터.js
@@ -16,7 +16,7 @@ function solution(priorities, location) {
             copy.push(first)
             // location 의 위치도 변경해주자
             if(find===0){
-                find +=  priorities.length-1
+                find =  copy.length-1
             }else {
                 find--
             }
@@ -34,3 +34,32 @@ function solution(priorities, location) {
 }
 
 console.log(solution([1, 1, 9, 1, 1, 1], 0), '결과')
+
+
+// 다른이 풀이
+function solution(priorities, location) { //중요도, 요청 문서 index
+    let target_index = location; //user가 선택한 index
+    let answer = 1;
+    let first = -1;
+    
+    while (priorities.length > 0) {
+        //1. 가장 앞에 있는 문서를 추출
+        first = priorities.shift();
+        //2. 중요도 높은 문서가 존재 시 뒤로 넣음
+        if (priorities.some((value, index) => value > first)) {
+            priorities.push(first);
+        }
+        else {
+            if (target_index === 0) {
+                break;
+            }
+            else answer ++;
+        }
+        /*3. 사용자가 선택한 문서가 중요도가 제일 높지 않은 경우
+             현재 대기목록의 맨 끝으로 index를 옮긴다.
+        */
+        if (target_index === 0) target_index = priorities.length - 1;
+        else target_index --;
+    }
+    return answer;
+}
