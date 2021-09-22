8c3f50c8390fc10f94519e85b810bd35d438de2f
sjs <seo777111@gmail.com>
2021-09-22 16:14:42 +0900

    https://programmers.co.kr/learn/courses/30/lessons/81301

--- 숫자 문자열과 영단어.js
+++ 숫자 문자열과 영단어.js
@@ -1,15 +1,17 @@
 // 다른이 풀이
-function solution(s) {
-    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var answer = s;
-
-    for(let i=0; i< numbers.length; i++) {
-        let arr = answer.split(numbers[i]);
-        answer = arr.join(i);
-    }
-
-    return Number(answer);
-}
+// function solution(s) {
+//     let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+//     var answer = s;
+//
+//     for(let i=0; i< numbers.length; i++) {
+//         let arr = answer.split(numbers[i]);
+//         console.log('numbers[i] :',numbers[i], 'arr: ',arr)
+//         answer = arr.join(i);
+//     }
+//
+//     return Number(answer);
+// }
+// solution("1zerotwozero3")
 
 function solution(s) {
     let charSet = {
@@ -31,44 +33,44 @@ function solution(s) {
     }
     return parseInt(s);
 }
-
-
-// 내풀이
-
-function solution(s) {
-    const card = {
-        0: 'zero',
-        1: 'one',
-        2: 'two',
-        3: 'three',
-        4: 'four',
-        5: 'five',
-        6: 'six',
-        7: 'seven',
-        8: 'eight',
-        9: 'nine',
-    }
-
-    let text= ''
-    let result =''
-
-
-    for(let str of s) {
-        text += str
-
-        for(let key in card){
-            if([card[key]].includes(text)) {
-                result += key;
-                text = '';
-                break;
-            }else if([key].includes(text)){
-                result += key;
-                text = '';
-                break;
-            }
-
-        }
-    }
-    console.log(result)
-    return parseInt(result,10);
-}
+//
+//
+// // 내풀이
+//
+// function solution(s) {
+//     const card = {
+//         0: 'zero',
+//         1: 'one',
+//         2: 'two',
+//         3: 'three',
+//         4: 'four',
+//         5: 'five',
+//         6: 'six',
+//         7: 'seven',
+//         8: 'eight',
+//         9: 'nine',
+//     }
+//
+//     let text= ''
+//     let result =''
+//
+//
+//     for(let str of s) {
+//         text += str
+//
+//         for(let key in card){
+//             if([card[key]].includes(text)) {
+//                 result += key;
+//                 text = '';
+//                 break;
+//             }else if([key].includes(text)){
+//                 result += key;
+//                 text = '';
+//                 break;
+//             }
+//
+//         }
+//     }
+//     console.log(result)
+//     return parseInt(result,10);
+// }
