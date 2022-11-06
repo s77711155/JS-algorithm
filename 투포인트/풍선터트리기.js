b722ee4ee4ef5e3e4c93c7a815395f3f72732f4f
SeoTrue <seo777111@gmail.com>
2022-11-06 17:40:57 +0900

    풍선터트리기 풀이

--- 투포인트/풍선터트리기.js
+++ 투포인트/풍선터트리기.js
@@ -43,3 +43,26 @@ function solution(arr) {
 
 let a = [ 5,7,1,8,9]
 console.log(solution(a))
+// 풀이참고 :https://dev-note-97.tistory.com/165, https://juyoungpark718.github.io/posts/127
+// 남의 풀이법
+
+
+
+function solution(a) {
+    let arr = Array(a.length).fill(1)
+    let minR = a[a.length-1]
+    let minL = a[0]
+    let ans = 0
+    for (let i = a.length-2;i>0;i--) {
+        if (a[i]<minR) minR = a[i]
+        else arr[i] = 0
+    }
+    for (let i = 0;i<a.length;i++){
+        if (a[i]<=minL) {
+            minL = a[i]
+            ans++
+        }
+        else if (arr[i]) ans++
+    }
+    return ans
+}
