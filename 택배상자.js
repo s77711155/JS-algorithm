62246b83b8cd8608bdbb897da784594bee66911f
SeoTrue <seo777111@gmail.com>
2022-12-28 22:06:32 +0900

    at 사용

--- 택배상자.js
+++ 택배상자.js
@@ -1,4 +1,27 @@
+function solution(order) {
+    let answer = 0;
+    const stack = [];
+    let idx = 0;
 
+    for (let i = 1; i <= order.length; i++) {
+        if (order[idx] !== i) {
+            stack.push(i);
+        } else {
+            idx++;
+            answer++;
+        }
+
+        while (stack.length !== 0 && stack.at(-1) === order[idx]) {
+            stack.pop();
+            idx++;
+            answer++;
+        }
+    }
+
+    return answer;
+}
+
+// 다른사람 풀이
 function solution(order) {
     let idx = 0;
     const stack = [];
@@ -9,6 +32,7 @@ function solution(order) {
             stack.pop();
             idx++;
         }
+        console.log(stack)
     }
     return idx;
 }
